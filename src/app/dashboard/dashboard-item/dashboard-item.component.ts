9fd085e7a3badae8f0646a40ce33ba3fd28a2a15
Julien Salvatore <julien.salvatore@gmail.com>
2024-07-24 17:28:58 +0200

    setup for extending built in elements

--- src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-item',
@@ -8,6 +8,8 @@ import { Component, Input } from '@angular/core';
   styleUrl: './dashboard-item.component.css',
 })
 export class DashboardItemComponent {
-  @Input({ required: true }) image!: { src: string; alt: string };
-  @Input({ required: true }) title!: string;
+  // @Input({ required: true }) image!: { src: string; alt: string };
+  // @Input({ required: true }) title!: string;
+  image = input.required<{ src: string; alt: string }>();
+  title = input.required<string>();
 }
