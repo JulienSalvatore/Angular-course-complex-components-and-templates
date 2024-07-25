d226f86d2163541252f6e2e3861385911fe7610c
Julien Salvatore <julien.salvatore@gmail.com>
2024-07-25 23:26:55 +0200

    make expend details for tickets

--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-import { NewTicketComponent } from "../new-ticket/new-ticket.component";
+import { Component, input, signal } from '@angular/core';
+import { NewTicketComponent } from '../new-ticket/new-ticket.component';
+import { Ticket } from '../ticket.model';
 
 @Component({
   selector: 'app-ticket',
   standalone: true,
   imports: [NewTicketComponent],
   templateUrl: './ticket.component.html',
-  styleUrl: './ticket.component.css'
+  styleUrl: './ticket.component.css',
 })
 export class TicketComponent {
+  data = input.required<Ticket>();
 
+  detailsVisible = signal(false);
+
+  onToggleDetails() {
+    // this.detailsVisible.set(!this.detailsVisible());
+    this.detailsVisible.update((wasVisible) => !wasVisible);
+  }
 }
